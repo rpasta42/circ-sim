8aeb60f080b8e3440c732d19a7e69b8832efee24
Konstantin Kowalski <kostelkow@gmail.com>
2017-04-26 13:51:37 +0000

    Figuring out Matrix code

---
+++ math.hs
@@ -0,0 +1,33 @@
+import qualified Data.Matrix as M
+
+--cabal install matrix
+--https://hackage.haskell.org/package/matrix-0.3.5.0/docs/Data-Matrix.html
+
+
+{-|
+
+m = |a b|
+    |c d|
+
+m = [[a, b], [c, d]]
+
+-}
+
+--data Matrix a = ListMatrix [[a]]
+
+
+
+
+
+--determinant :: (Num a) => [[a]] -> a
+--determinant xs = determinant' (M.fromLists xs)
+
+
+--determinant' :: (Num a) => Matrix a -> a
+--determinant'
+
+
+--bad
+--determinant [] = error "empty matrix"
+--determinant [[x]] =
+
