c6b889fa523fb1530cf13e8d8acb16adca635e23
Konstantin Kowalski <kostelkow@gmail.com>
2017-04-28 15:00:08 +0000

    Improved OpenGL code
    Drawing Circle with renderPolygon

--- GlDrawer.hs
+++ GlDrawer.hs
@@ -4,8 +4,22 @@
 
 import Graphics.UI.GLUT
 
+type GlVert = [(GLfloat, GLfloat, GLfloat)]
+
+getCirclePoints :: Float -> GlVert
+getCirclePoints n = [ (sin (2*pi*k/n), cos (2*pi*k/n), 0) | k <- [1..n] ]
+
 myPoints :: [(GLfloat, GLfloat, GLfloat)]
-myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+myPoints = getCirclePoints 100
+
+
+-- renderMode (renderPrimitive) types
+--Points/Triangles/TriangleStrip/TriangleFan/Lines/
+--LineLoop/LineStrip/Quads/QuadStrip/Polygon
+drawVertices :: (Foldable t1, VertexComponent a) => t -> t1 (a, a, a) -> IO ()
+drawVertices renderMode vertices = renderPrimitive Polygon $
+   mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) vertices
+
 
 main :: IO ()
 main = do
@@ -14,10 +28,10 @@ main = do
   displayCallback $= display
   mainLoop
 
+
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
-  renderPrimitive Points $
-   mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  drawVertices Polygon myPoints
   flush
 
