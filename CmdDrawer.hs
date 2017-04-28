2795c1fce8dc6df764d4b74db0f6eb7da4a3a2df
Konstantin Kowalski <kostelkow@gmail.com>
2017-04-28 14:50:55 +0000

    Drawing points with OpenGL

--- CmdDrawer.hs
+++ CmdDrawer.hs
@@ -3,9 +3,6 @@ module CmdDrawer (
 ) where
 
 
----sudo apt-get install freeglut3 freeglut3-dev
----sudo apt install cabal; cabal update; cabal install glut
---mkdir /tmp/notes_serv; sshfs root@69.164.203.126:/ /tmp/notes_serv
 
 
 import Circuit
@@ -17,4 +14,4 @@ drawCircuit (Circuit elements) = drawCircuit' elements
 drawCircuit' :: [(CircuitElement a, DrawData b)]
 
 drawCircuit' [] = ""
-drawCircuit' (x:[]) =
+drawCircuit' (x:[]) = ""
--- GlDrawer.hs
+++ GlDrawer.hs
@@ -1,7 +1,11 @@
 ---sudo apt-get install freeglut3 freeglut3-dev
 ---sudo apt install cabal; cabal update; cabal install glut
 --mkdir /tmp/notes_serv; sshfs root@69.164.203.126:/ /tmp/notes_serv
+
 import Graphics.UI.GLUT
+
+myPoints :: [(GLfloat, GLfloat, GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
 
 main :: IO ()
 main = do
@@ -13,5 +17,7 @@ main = do
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
+  renderPrimitive Points $
+   mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
   flush
 
