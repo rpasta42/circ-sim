04df8d442240780b62c2a1d1402f44e971fe2d95
Konstantin Kowalski <kostelkow@gmail.com>
2017-04-28 16:25:04 +0000

    Working on findAdjacent for path finder

--- PathFinder.hs
+++ PathFinder.hs
@@ -2,11 +2,12 @@ import Data.List
 import qualified Data.Matrix as M
 
 
+type TileMatrix a = M.Matrix a
 type TileMap a = [[a]]
 type Coord = (Int, Int, Int)
 
 getTileMap :: TileMap Char
-getTileMap = [
+getTileMap = [   --y
    "xxxxxxxxxx", --0
    "x...xx.x.x", --1
    "x.x..x...x", --2
@@ -18,7 +19,7 @@ getTileMap = [
    "x..o.x...x", --8
    "xxxxxxxxxx"  --9
    ]
-
+----0123456789 x
 
 findTile :: (Eq a) => a -> TileMap a -> Maybe Coord
 findTile destChar tileMap = findTile' destChar tileMap 0
@@ -41,13 +42,23 @@ findPath tileMap startChar endChar emptyChar fullChar =
    in case (startPos, endPos) of
       (Nothing, _) => Right "No starting position"
       (_, Nothing) => Right "No end position"
-      _ => findPath' tileMap startChar endChar emptyChar fullChar
+      _ => findPath' (M.fromLists tileMap) startChar endChar emptyChar fullChar
 
-findPath' :: (Eq a) => TileMap a -> a -> a -> a -> Either [Coord] String
+findPath' :: (Eq a) => TileMatrix a -> a -> a -> a -> Either [Coord] String
 findPath' tileMap startChar endChar emptyChar fullChar =
 
 
-
-
+findAdjacent' :: (Eq a) => TileMatrix a -> Coord -> [Coord]
+findAdjacent' tileMap adjacentTo@(x, y, z) emptyChar fullChar =
+   let topLeft = (x-1, y-1, z+1)
+       midLeft = (x-1, y,   z+1)
+       botLeft = (x-1, y+1, z+1)
+       topMid  = (x,   y-1, z+1)
+       botMid  = (x,   y+1, z+1)
+       topRight= (x+1, y-1, z+1)
+       midRight= (x+1, y,   z+1)
+       botRight= (x+1, y+1, z+1)
+   in filterBad [topLeft, midLeft, botLeft, toMid, botMid, topRight, midRight, botRight]
+      where filterBad lst =
 
 
